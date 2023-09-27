aee32d350a1b93fe465262cb8e0bbe2eb0c5c4e4
Felipe <felipe.almeida@domvsit.com.br>
2023-09-26 21:06:54 -0300

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PDIv2.Models
+{
+    public class User : IdentityUser
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }     
+    }
+}
